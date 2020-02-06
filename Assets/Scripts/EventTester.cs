a028cae04eaaeebd601cdd2b9d5780b2000288c1
Lenophie <quasiphies@gmail.com>
2020-02-06 11:58:06 +0100

    Add UnityEvent demo

---
+++ Assets/Scripts/EventTester.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class EventTester : MonoBehaviour {
+    public void Test() {
+        Debug.Log("call me !");
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
+    [SerializeField] private UnityEvent onStartEvent = default;
+
     public void OnStart() {
+        onStartEvent.Invoke();
         SceneManager.LoadScene("SampleScene");
     }
 
