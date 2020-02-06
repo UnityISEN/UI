96810f8ad1a8332e365dc2e007073b06ebe0615c
Lenophie <quasiphies@gmail.com>
2020-02-06 11:49:07 +0100

    Add SceneController

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public void OnStart() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
